ed681645402b4082b285605470004c2cc7930f68
C. James Winslow <whoaa512@gmail.com>
2014-10-02 18:11:04 -0700

    fixed gulp bower task by update to main-bower-files

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp');
-var bowerFiles = require('gulp-bower-files');
+var bowerFiles = require('main-bower-files');
 var filter = require('gulp-filter');
 var flatten = require('gulp-flatten');
 var size = require('gulp-size');
 
 gulp.task('fonts', function() {
-  return bowerFiles()
+  return gulp.src(bowerFiles())
     .pipe(filter('**/*.{eot,svg,ttf,woff}'))
     .pipe(flatten())
     .pipe(gulp.dest('dist/fonts'))
