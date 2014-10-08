02086e22a6bb2371a0906e300421bbc4ae210881
C. James Winslow <whoaa512@gmail.com>
2014-10-08 13:36:21 -0700

    fixed subtle bug in html task related to new interface for useref

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -9,16 +9,17 @@ var size = require('gulp-size');
 gulp.task('html', ['styles', 'browserify'], function() {
   var jsFilter = filter('**/*.js');
   var cssFilter = filter('**/*.css');
+  var assets = useref.assets({ searchPath: '{.tmp,app}' })
 
   return gulp.src('app/*.html')
-    .pipe(useref.assets({ searchPath: '{.tmp,app}' }))
+    .pipe(assets)
     .pipe(jsFilter)
     .pipe(uglify())
     .pipe(jsFilter.restore())
     .pipe(cssFilter)
     .pipe(csso())
     .pipe(cssFilter.restore())
-    .pipe(useref.assets().restore())
+    .pipe(assets.restore())
     .pipe(useref())
     .pipe(gulp.dest('dist'))
     .pipe(size())
